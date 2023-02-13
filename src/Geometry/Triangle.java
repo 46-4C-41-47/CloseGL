c0d286b2ff3d9834ea92a5069935590c4f4569bb
46-4C-41-47 <laptop@gmail.com>
2023-02-13 23:07:06 +0100

    m

--- src/Geometry/Triangle.java
+++ src/Geometry/Triangle.java
@@ -1,4 +1,18 @@
 package Geometry;
 
 public class Triangle {
+    public int x, y, z;
+
+    public Triangle() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+
+    public Triangle(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
