587f5ea491e8b4f696c8da0c8163ff6bc962a4e4
Romain Besson <122835124+46-4C-41-47@users.noreply.github.com>
2023-03-10 09:12:43 +0100

    Add files via upload

--- src/Application/DrawTask.java
+++ src/Application/DrawTask.java
@@ -12,7 +12,7 @@ public class DrawTask extends TimerTask {
     private final Mesh cube;
     private final Screen screen;
     //private final Matrix projMatrix = Main.initProjectionMatrix();
-    private float elapsedTime = 0f;
+    private float elapsedTime = (float) ((1000 / (float) Parameters.FRAME_RATE) * 0.001) * 72f;
 
 
     public DrawTask(Canvas canvas, Mesh cube, Screen screen) {
@@ -36,9 +36,8 @@ public class DrawTask extends TimerTask {
 
         float x = line.getPoint().x + line.getVector().x * t;
         float y = line.getPoint().y + line.getVector().y * t;
-        float z = 0;
 
-        return new Point3D(x, y, z);
+        return new Point3D(x, y, 0f);
     }
 
 
@@ -76,57 +75,74 @@ public class DrawTask extends TimerTask {
         return shiftedMesh;
     }
 
-/*
-    private Mesh computeProjection(Mesh object) {
-        Mesh projectedObject = new Mesh();
 
-        for (Triangle triangle : object.getTriangles()) {
-            Triangle projectedTriangle = new Triangle(
-                    triangle.points[0],
-                    triangle.points[1],
-                    triangle.points[2]
-            );
-
-            projectedTriangle = scalePoints(projectedTriangle);
-            projectedObject.add(projectedTriangle);
-        }
-
-        return projectedObject;
-    }
-*/
-
-    private Mesh computeRotation(Mesh object) {
+    private Mesh rotateX(Mesh object, float theta) {
         Mesh rotatedObject = new Mesh();
-        Matrix rotMatZ = new Matrix(), rotMatX = new Matrix();
+        Matrix rotationMatrixX = new Matrix();
 
-        // Rotation Z
-        rotMatZ.matrix[0][0] = (float)  Math.cos(elapsedTime);
-        rotMatZ.matrix[0][1] = (float)  Math.sin(elapsedTime);
-        rotMatZ.matrix[1][0] = (float) -Math.sin(elapsedTime);
-        rotMatZ.matrix[1][1] = (float)  Math.cos(elapsedTime);
-        rotMatZ.matrix[2][2] = 1;
-        rotMatZ.matrix[3][3] = 1;
-
-        // Rotation X
-        rotMatX.matrix[0][0] = 1;
-        rotMatX.matrix[1][1] = (float)  Math.cos(elapsedTime * 0.5f);
-        rotMatX.matrix[1][2] = (float)  Math.sin(elapsedTime * 0.5f);
-        rotMatX.matrix[2][1] = (float) -Math.sin(elapsedTime * 0.5f);
-        rotMatX.matrix[2][2] = (float)  Math.cos(elapsedTime * 0.5f);
-        rotMatX.matrix[3][3] = 1;
+        rotationMatrixX.matrix[0][0] = 1;
+        rotationMatrixX.matrix[1][1] = (float)  Math.cos(theta);
+        rotationMatrixX.matrix[1][2] = (float)  Math.sin(theta);
+        rotationMatrixX.matrix[2][1] = (float) -Math.sin(theta);
+        rotationMatrixX.matrix[2][2] = (float)  Math.cos(theta);
+        rotationMatrixX.matrix[3][3] = 1;
 
         for (Triangle triangle : object.getTriangles()) {
             Triangle rotatedTriangle = new Triangle(
-                    Main.multiplyMatrixPoint(triangle.points[0], rotMatZ),
-                    Main.multiplyMatrixPoint(triangle.points[1], rotMatZ),
-                    Main.multiplyMatrixPoint(triangle.points[2], rotMatZ)
+                    Main.multiplyMatrixPoint(triangle.points[0], rotationMatrixX),
+                    Main.multiplyMatrixPoint(triangle.points[1], rotationMatrixX),
+                    Main.multiplyMatrixPoint(triangle.points[2], rotationMatrixX)
             );
 
-            /*rotatedTriangle = new Triangle(
-                    Main.multiplyMatrixPoint(rotatedTriangle.points[0], rotMatX),
-                    Main.multiplyMatrixPoint(rotatedTriangle.points[1], rotMatX),
-                    Main.multiplyMatrixPoint(rotatedTriangle.points[2], rotMatX)
-            );*/
+            rotatedObject.add(rotatedTriangle);
+        }
+
+        return rotatedObject;
+    }
+
+
+    private Mesh rotateY(Mesh object, float theta) {
+        Mesh rotatedObject = new Mesh();
+        Matrix rotationMatrixY = new Matrix();
+
+        rotationMatrixY.matrix[0][0] = (float)  Math.cos(theta);
+        rotationMatrixY.matrix[1][1] = 1;
+        rotationMatrixY.matrix[2][0] = (float)  Math.sin(theta);
+        rotationMatrixY.matrix[0][2] = (float) -Math.sin(theta);
+        rotationMatrixY.matrix[2][2] = (float)  Math.cos(theta);
+        rotationMatrixY.matrix[3][3] = 1;
+
+        for (Triangle triangle : object.getTriangles()) {
+            Triangle rotatedTriangle = new Triangle(
+                    Main.multiplyMatrixPoint(triangle.points[0], rotationMatrixY),
+                    Main.multiplyMatrixPoint(triangle.points[1], rotationMatrixY),
+                    Main.multiplyMatrixPoint(triangle.points[2], rotationMatrixY)
+            );
+
+            rotatedObject.add(rotatedTriangle);
+        }
+
+        return rotatedObject;
+    }
+
+
+    private Mesh rotateZ(Mesh object, float theta) {
+        Mesh rotatedObject = new Mesh();
+        Matrix rotationMatrixZ = new Matrix();
+
+        rotationMatrixZ.matrix[0][0] = (float)  Math.cos(theta);
+        rotationMatrixZ.matrix[0][1] = (float)  Math.sin(theta);
+        rotationMatrixZ.matrix[1][0] = (float) -Math.sin(theta);
+        rotationMatrixZ.matrix[1][1] = (float)  Math.cos(theta);
+        rotationMatrixZ.matrix[2][2] = 1;
+        rotationMatrixZ.matrix[3][3] = 1;
+
+        for (Triangle triangle : object.getTriangles()) {
+            Triangle rotatedTriangle = new Triangle(
+                    Main.multiplyMatrixPoint(triangle.points[0], rotationMatrixZ),
+                    Main.multiplyMatrixPoint(triangle.points[1], rotationMatrixZ),
+                    Main.multiplyMatrixPoint(triangle.points[2], rotationMatrixZ)
+            );
 
             rotatedObject.add(rotatedTriangle);
         }
@@ -137,10 +153,10 @@ public class DrawTask extends TimerTask {
 
     public Triangle scalePoints(Triangle triangle) {
         for (Point3D point: triangle.points) {
-            point.x += 2;
             point.x *= (float) Parameters.FRAME_SIZE.width  / Parameters.SCALE_FACTOR;
-            point.y += 2;
-            point.y *= (float) Parameters.FRAME_SIZE.width / Parameters.SCALE_FACTOR;
+            point.x += (float) Parameters.FRAME_SIZE.width  / 2;
+            point.y *= (float) Parameters.FRAME_SIZE.height / Parameters.SCALE_FACTOR;
+            point.y += (float) Parameters.FRAME_SIZE.height / 2;
         }
 
         return triangle;
@@ -149,11 +165,13 @@ public class DrawTask extends TimerTask {
 
     @Override
     public void run() {
-        elapsedTime += (1000 / (float) Parameters.FRAME_RATE) * 0.001;
+        elapsedTime += (1000 / (float) Parameters.FRAME_RATE) * 0.0005;
         //System.out.println(cube);
         Mesh modifiedCube = new Mesh(cube);
-        modifiedCube = shiftMesh(modifiedCube,2);
-        modifiedCube = computeRotation(modifiedCube);
+        modifiedCube = shiftMesh(modifiedCube,3);
+        modifiedCube = rotateX(modifiedCube, elapsedTime);
+        modifiedCube = rotateY(modifiedCube, elapsedTime * 0.33f);
+        modifiedCube = rotateZ(modifiedCube, elapsedTime * 0.66f);
 
         List<Mesh> projectedObjects = new ArrayList<>();
         projectedObjects.add(computeProjection(modifiedCube));
--- src/Application/Parameters.java
+++ src/Application/Parameters.java
@@ -7,10 +7,10 @@ import java.awt.Dimension;
 import java.awt.Color;
 
 public class Parameters {
-    static final Dimension FRAME_SIZE = new Dimension(800, 600);
+    static final Dimension FRAME_SIZE = new Dimension(1000, 563);
     static final int FRAME_RATE = 60;
-    static final int SCALE_FACTOR = 20;
-    static final float ZOOM_FACTOR = 1;
+    static final int SCALE_FACTOR = 10;
+    static final float ZOOM_FACTOR = 1f;
     static final float FOV = 90;
     static final float SCREEN_RATIO = 16f/9;
     static final Vector3D SCREEN_PLANE_VECTOR = new Vector3D(0, 0, 1);
